5abb9cfdb99cbcfd283b0b71c29b4a4b08d961fa
camilo puentes <jcpsandoval94@gmail.com>
2024-06-06 13:05:10 -0500

    feat: Add Vehiculo and Accesorio classes
    This commit adds the Vehiculo and Accesorio classes to the CleanArchitecture.Domain.Vehiculos namespace. The Vehiculo class is a sealed class representing a vehicle, with properties such as Id, Modelo, Vin, and various location details. The Accesorio enum represents different accessories that can be associated with a vehicle, such as wifi, AireAcondicionado, AppleCar, AndroidCar, and Mapas.

---
+++ src/ClearArchitecture/CleanArchitecture.Domain/Vehiculos/Accesorio.cs
@@ -0,0 +1,10 @@
+namespace CleanArquitecture.Domain.vehiculos;
+
+public enum Accesorio
+{
+    wifi = 1,
+    AireAcondicionado = 2,
+    AppleCar = 3,
+    AndroidCar = 4,
+    Mapas = 5,
+}
---
+++ src/ClearArchitecture/CleanArchitecture.Domain/Vehiculos/Vehiculo.cs
@@ -0,0 +1,20 @@
+namespace CleanArquitecture.Domain.vehiculos;
+
+public sealed class Vehiculo
+{
+	public Guid Id { get; private set; }
+	public string? Modelo { get; private set; }
+	public string? Vin { get; private set; }
+	public string? Calle { get; private set; }
+	public string? Departamento { get; private set; }
+	public string? Municipio { get; private set; }
+	public string? Ciudad { get; private set; }
+	public string? Pais { get; private set; }
+	public decimal Precio { get; private set; }
+	public string? TipoMoneda { get; private set; }
+	public decimal Mantenimiento { get; private set; }
+	public string? MantenimientoTipoModena { get; private set; }
+	public DateTime? FechaUltimoAlquiler { get; private set; }
+	public List<Accesorio> Accesorios { get; private set; } = new();
+
+}
