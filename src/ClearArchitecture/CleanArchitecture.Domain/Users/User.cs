302e7e4a611e911e34cd04671eb479282ecf2eb9
camilo puentes <jcpsandoval94@gmail.com>
2024-06-10 16:59:11 -0500

    Add User domain entity

---
+++ src/ClearArchitecture/CleanArchitecture.Domain/Users/User.cs
@@ -0,0 +1,38 @@
+using CleanArquitecture.Domain.Abstractions;
+
+namespace CleanArquitecture.Domain.Users;
+
+public sealed class User : Entity
+{
+    private User(
+        Guid id,
+        Nombre nombre,
+        Apellido apellido,
+        Email email
+        ) : base(id)
+    {
+        Nombre = nombre;
+        Apellido = apellido;
+        Email = email;
+    }
+
+    public Nombre? Nombre { get; private set; }
+    public Apellido? Apellido { get; private set; }
+    public Email? Email { get; private set; }
+
+    public static User Create(
+        Nombre nombre,
+        Apellido apellido,
+        Email email
+        )
+    {
+        var user = new User(
+            Guid.NewGuid(),
+            nombre,
+            apellido,
+            email
+        );
+
+        return user;
+    }
+}
